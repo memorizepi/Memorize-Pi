7a4a8f78b11e1107ab5377af534296c32c90e132
memorizepi <160355997+memorizepi@users.noreply.github.com>
2024-02-23 23:33:31 -0600

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,82 +1,139 @@
 const arr = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116093305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989"
+const arrDigits = arr.split("");
 var digits = 0
 var wyg = ""
 var correct = 0
 var incorrect = 0
 var skipped = 0
+var atleastihavethis = 8
 var colors = []
 var lostelems = []
 const divs = document.getElementById("output")
 const pad = document.getElementById("numberPad")
 const pop = document.getElementById("popup")
 const score = document.getElementById("score")
-function func(num){
-    if(arr[digits] == num){
-        divs.innerHTML += "<h1 style='font-size:35px' class='green'>"+num+"</h1>"
-        digits++
-        correct++
-        score.innerHTML = "digits: " + digits
-        wyg = wyg + String(num)
-        console.log(wyg.length)
-        if(wyg.length > 9){
-            var lost = divs.firstChild
-            divs.removeChild(divs.firstElementChild)
-            lostelems.push(lost)
-            console.log(lostelems)
-        }
-        console.log("correct: "+ correct)
-        document.getElementById("corr").innerHTML = "Correct digits: "+correct
-        colors.push("green")
-    } else {
-        divs.innerHTML += "<h1 style='font-size:35px' class='red'>"+arr[digits]+"</h1>"
-        digits++
-        incorrect++
-        score.innerHTML = "digits: " + digits
-        document.getElementById("inco").innerHTML = "Incorrect digits: "+incorrect
-        wyg = wyg + String(num)
-        console.log(wyg.length)
-        if(wyg.length > 9){
-            divs.removeChild(divs.firstElementChild)
-        }
-        colors.push("red")
+function func(num) {
+  if (arr[digits] == num) {
+    if (digits > 7) {
+      divs.removeChild(divs.firstElementChild)
     }
-}
-function skip(){
-    divs.innerHTML += "<h1 style='font-size:35px' class='yellow'>"+arr[digits]+"</h1>"
+    divs.innerHTML += "<h1 style='font-size:35px' class='green'>" + num + "</h1>"
     digits++
-    skipped++
+    correct++
     score.innerHTML = "digits: " + digits
-    document.getElementById("skip").innerHTML = "Skipped digits: "+skipped
-    wyg = wyg + String(arr[digits])
+    wyg = wyg + String(num)
     console.log(wyg.length)
-    if(wyg.length > 9){
-        divs.removeChild(divs.firstElementChild)
+    console.log("correct: " + correct)
+    document.getElementById("corr").innerHTML = "Correct digits: " + correct
+    colors.push("green")
+  } else {
+    if (digits > 6) {
+      divs.removeChild(divs.firstElementChild)
     }
-    colors.push("yellow")
+    divs.innerHTML += "<h1 style='font-size:35px' class='red'>" + arr[digits] + "</h1>"
+    digits++
+    incorrect++
+    score.innerHTML = "digits: " + digits
+    document.getElementById("inco").innerHTML = "Incorrect digits: " + incorrect
+    wyg = wyg + String(num)
+    console.log(wyg.length)
+
+    colors.push("red")
+  }
+}
+function skip() {
+  if (digits > 6) {
+    divs.removeChild(divs.firstElementChild)
+  }
+  divs.innerHTML += "<h1 style='font-size:35px' class='yellow'>" + arr[digits] + "</h1>"
+  digits++
+  skipped++
+  score.innerHTML = "digits: " + digits
+  document.getElementById("skip").innerHTML = "Skipped digits: " + skipped
+  wyg = wyg + String(arr[digits])
+  console.log(wyg.length)
+  colors.push("yellow")
 }
 function back() {
-    divs.removeChild(divs.lastElementChild);
-    digits--;
-    wyg = wyg.slice(0, wyg.length - 1);
-    console.log(wyg);
-    score.innerHTML = "digits: " + digits;
+  if (digits > 8) {
+    const firstChild = arr[digits - 9];
+    console.log("YAYYYY:   " + firstChild)
+    divs.removeChild(divs.lastElementChild)
+    digits--
+    score.innerHTML = "digits: " + digits
     if (colors[digits] == "green") {
-      correct--;
-      document.getElementById("corr").innerHTML = "Correct digits: " + correct;
+      correct--
+      document.getElementById("corr").innerHTML = "Correct digits: " + correct
     } else if (colors[digits] == "red") {
-      incorrect--;
-      document.getElementById("inco").innerHTML = "Incorrect digits: " + incorrect;
+      incorrect--
+      document.getElementById("inco").innerHTML = "Incorrect digits: " + incorrect
     } else if (colors[digits] == "yellow") {
-      skipped--;
-      document.getElementById("skip").innerHTML = "Skipped digits: " + skipped;
+      skipped--
+      document.getElementById("skip").innerHTML = "Skipped digits: " + skipped
     }
-    colors.pop();
-    if (wyg.length > 9) {
-      var lostChild = divs.firstElementChild;
-      lostelems.push(lostChild);
-      divs.removeChild(lostChild);
-      var newChild = lostelems[lostelems.length - 1];
-      divs.insertBefore(newChild, divs.firstElementChild);
+    colors.pop()
+    if (digits > 7) {
+      var htmls = divs.innerHTML
+      if (firstChild != undefined) {
+        divs.innerHTML = "<h1 style='font-size:35px; position: relative' class='" + colors[digits - 8] + "'>" + firstChild + "</h1>";
+        console.log("child: ", firstChild)
+        divs.innerHTML += htmls
+      }
+      //`<h1 class='${colors[digits - 8]}'>${htmls}</h1>`
+    } else {
+      console.log(wyg)
     }
+  } else {
+    digits = 0
+
+    divs.innerHTML = ""
+    incorrect = 0
+    var incorrect = 0
+    var skipped = 0
+    document.getElementById("corr").innerHTML = "Correct digits: " + correct
+    document.getElementById("inco").innerHTML = "Incorrect digits: " + incorrect
+    document.getElementById("skip").innerHTML = "Skipped digits: " + skipped
+    score.innerHTML = "digits: " + digits
   }
+}
+
+window.addEventListener("keydown", mykeydown);
+
+function mykeydown(e) {
+  keyPressed = e.keyCode;
+  console.log("key: " + keyPressed);
+  if (keyPressed == "49") {
+    func("1")
+  }
+  if (keyPressed == "50") {
+    func("2")
+  }
+  if (keyPressed == "51") {
+    func("3")
+  }
+  if (keyPressed == "52") {
+    func("4")
+  }
+  if (keyPressed == "53") {
+    func("5")
+  }
+  if (keyPressed == "54") {
+    func("6")
+  }
+  if (keyPressed == "55") {
+    func("7")
+  }
+  if (keyPressed == "56") {
+    func("8")
+  }
+  if (keyPressed == "57") {
+    func("9")
+  }
+  if (keyPressed == "48") {
+    func("0")
+  }
+  if (keyPressed == "190") {
+    func(".")
+  }
+}
 
